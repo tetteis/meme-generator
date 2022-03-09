80800055df3dad179645309ad7416e15306f4ff1
Tettei Shahday-Annang <tetteis@gmail.com>
2022-03-09 22:26:17 +0000

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="" alt="logo" />
+      <h2>Header component</h2>
+      <h4>Meme Generator</h4>
+    </div>
+  );
+}
